b01a31f0fc3a23291f01c9b0105c752cb2e460a2
Soham Jaiswal <sosweetham@gmail.com>
2024-07-20 15:11:55 +0530

    fix: json key names

--- prices/prices.go
+++ prices/prices.go
@@ -8,10 +8,10 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager filemanager.FileManager
-	TaxRate float64
-	InputPrices []float64
-	TaxIncludedPrices map[string]string
+	IOManager filemanager.FileManager `json:"-"`
+	TaxRate float64 `json:"tax_rate"`
+	InputPrices []float64 `json:"input_prices"`
+	TaxIncludedPrices map[string]string `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) GetInputPrices() {
